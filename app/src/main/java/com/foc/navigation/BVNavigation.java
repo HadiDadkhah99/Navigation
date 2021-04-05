104b9c91c40a17d0ef7294068af5e5a95d2458c7
hadidadkhah <hadidadkhah99@gmail.com>
2021-04-05 20:27:28 +0430

    commit

---
+++ app/src/main/java/com/foc/navigation/BVNavigation.java
@@ -0,0 +1,55 @@
+package com.foc.navigation;
+
+
+import android.view.MenuItem;
+
+import com.foc.libs.pronavigation.interfaces.ItemClicked;
+import com.foc.libs.pronavigation.utils.BottomNavigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+
+
+public class BVNavigation extends BottomNavigation {
+
+    private final BottomNavigationView bottomNavigationView;
+
+    public BVNavigation(BottomNavigationView bottomNavigationView) {
+        this.bottomNavigationView = bottomNavigationView;
+    }
+
+    @Override
+    public void setOnItemSelected(ItemClicked itemClicked) {
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id._home)
+                itemClicked.clicked(0);
+            else if (item.getItemId() == R.id._shop)
+                itemClicked.clicked(1);
+            else if (item.getItemId() == R.id._profile)
+                itemClicked.clicked(2);
+
+            return true;
+        });
+
+    }
+
+    @Override
+    public void setItemActiveIndex(int pos) {
+
+        switch (pos) {
+            case 0:
+                bottomNavigationView.setSelectedItemId(R.id._home);
+                break;
+            case 1:
+                bottomNavigationView.setSelectedItemId(R.id._shop);
+                break;
+            case 2:
+                bottomNavigationView.setSelectedItemId(R.id._profile);
+                break;
+        }
+
+    }
+
+}
--- app/src/main/java/com/foc/navigation/MainActivity.java
+++ app/src/main/java/com/foc/navigation/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.foc.libs.pronavigation.NavigationPro;
+import com.foc.navigation.fragment.HomeFragment;
+import com.foc.navigation.fragment.ProfileFragment;
+import com.foc.navigation.fragment.ShopFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView=findViewById(R.id.bottom_view);
+        bottomNavigationView = findViewById(R.id.bottom_view);
 
         //init nav
         this.navigationPro = NavigationPro.init(this, R.id.main_frame);
+        this.navigationPro.
+                addFragment(HomeFragment.class, true).
+                addFragment(ShopFragment.class, true).
+                addFragment(ProfileFragment.class, true);
 
+
+        this.navigationPro.attachBottomNavigation(new BVNavigation(bottomNavigationView));
     }
 }
